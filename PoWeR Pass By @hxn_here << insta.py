b4f4f5c3fb3ebb15b621255f4dc0aee31686494f
HEXiN @hxn_here  insta <98489657+HXN2@users.noreply.github.com>
2022-01-28 13:27:54 +0300

    Create PoWeR Pass By @hxn_here << insta.py

---
+++ PoWeR Pass By @hxn_here << insta.py
@@ -0,0 +1,108 @@
+import os
+
+import webbrowser
+
+url = "https://t.me/HEXiN1K"
+
+webbrowser.open(url)
+
+import socket
+
+name = socket.gethostname()
+
+print("\n===========================================================")
+
+print("\n                    Hello sir " +name) 
+
+print("\n===========================================================")
+
+lib = input("""
+
+\n  من صنع هيكسن انستجرام @hxn_here
+
+\n  BY HeXiN INSTA @hxn_here
+
+\n  tarafından yapılmış HeXiN Instagram @hxn_here
+
+\n
+
+===========================================================
+
+\n   من صنع هيكسن تيليجرام @HEXiN 
+
+\n  BY HEXiN TELeGRaM @HEXiN1K
+
+\n  Bir telgraf HEXiN oluşturun @HEXiN1K
+
+\n ===========================================================
+
+ [1] DOWNLOAD LiP & UPDATE . تنزيل وتحديث المكاتب
+
+\n [2] SKiP DOWNLOAD & UPDATE LiP . تخطي تنزيل وتحديث المكاتب 
+
+\n [+] Please Choice >> """)
+
+if lib == "1":
+
+    try:
+
+        os.system('pip install webbrowser')
+
+        os.system('pip install choice')
+
+        os.system('pip install socket')
+
+        os.system('pip install random')
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        pass
+
+    except:
+
+        pass
+
+else:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    pass
+
+import socket
+
+import webbrowser
+
+import os
+
+from random import choice
+
+def createpass(data):
+
+    password = input ("Enter password:")
+
+    char = f"@#$&@#$&{password}abcdefghijklmnopqrstuvwxyzASDFGHJKLZXCVBNM0123456789"
+
+    for i in range(data):
+
+        password += choice(char)
+
+    return password
+
+print(createpass(8))
+
+import webbrowser
+
+url = "https://www.instagram.com/hxn_here/"
+
+webbrowser.open(url)
+
+import socket
+
+name = socket.gethostname()
+
+print("\n===========================================================")
+
+print("\n                    Bye sir " +name) 
+
+print("\n===========================================================")
+
